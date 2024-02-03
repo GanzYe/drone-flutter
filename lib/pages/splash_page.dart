6172f9ab52b106951f51389ca404f73083ea801d
Shakhzod Bakhadirov <bahadirov4@gmail.com>
2024-02-03 20:50:26 +0100

    add: Splash Page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,50 @@
+import 'package:drone/bloc/drones_details_cubit/drones_details_cubit.dart';
+import 'package:drone/config/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  static MaterialPageRoute<void> route() => MaterialPageRoute<void>(
+        builder: (_) => const SplashPage(),
+      );
+
+  // void _navigateToIntro(BuildContext context) {
+  //   Navigator.of(context).pushReplacement(
+  //     IntroPage.route(),
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) =>
+      BlocListener<DronesDetailsCubit, DronesDetailsState>(
+        listener: (context, state) {
+          if (state.status == FormzSubmissionStatus.success) {}
+        },
+        child: Scaffold(
+          backgroundColor: AppTheme.splashBG,
+          body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Drone',
+                  style: AppTheme.titleLargeBold500
+                      .copyWith(color: AppTheme.mainColor),
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  'Map',
+                  style: AppTheme.titleLargeBold700
+                      .copyWith(color: AppTheme.secondColor),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+}
